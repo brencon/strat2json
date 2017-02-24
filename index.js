7bcbbc32a4f2fb4627f00ce3984a027c67bd5793
Brendan Conrad <brconrad@symitar.com>
2017-02-23 21:39:36 -0800

    Started determination of column headers and statistics for batters

--- index.js
+++ index.js
@@ -16,16 +16,66 @@ module.exports = {
     primaryStats2json: function(primaryStatsPRT) {
         var lines = primaryStatsPRT.split(/\r?\n/);
         var jsonStats = { "teams": [] };
+        var teamLineFound = false;
+        var statsHeaderLineParsed = false;
+        var battingColumns = [];
+        var teamIndex = -1;
+        const primaryPlayerStatistics = 'Primary Player Statistics For';
+        const regExpBracesAndOne = /\[[1]\]/;
+        const regExpBracesAndTwo = /\[[2]\]/;
         _.forEach(lines, function(line) {
             // find each team based on "Primary Player Statistics"
-            var primaryPlayerStatistics = 'Primary Player Statistics For 2016 ';
             if (line.indexOf(primaryPlayerStatistics) > 0) {
-                var teamLine = line.substr(38);
+                teamLineFound = true;
+                var teamLine = line.substr(primaryPlayerStatistics.length + 9);
                 var totalsAfterGames = ' Totals After';
                 var team = teamLine.substr(0, teamLine.indexOf(totalsAfterGames));
                 jsonStats.teams.push({"team": team});
+                teamIndex++;
+                jsonStats.teams[teamIndex].batters = [];
+                jsonStats.teams[teamIndex].pitchers = [];
             }
+            if (teamLineFound === false) {
+                // determine if the line is empty, a stats header row, or player row
+                if (line !== '') {
+                    var statsHeaderLine = (line.search(regExpBracesAndOne) && line.indexOf('NAME') > 0);
+                    if ((statsHeaderLine !== false) && (statsHeaderLineParsed === false)) {
+                        // parse stats header line once
+                        line = line.replace(regExpBracesAndOne, '');
+                        var headerLineColumnHeadings = line.split(/(\s+)/);
+                        _.forEach(headerLineColumnHeadings, function(columnHeading) {
+                           if (columnHeading.trim() !== '') {
+                               battingColumns.push(columnHeading);
+                           }
+                        });
+                        statsHeaderLineParsed = true;
+                    }
+                    if (statsHeaderLine === false) {
+                        // if the line is not [2]
+                        if (line.search(regExpBracesAndTwo) === -1) {
+                            // then it's a stats line
+                            var statsColumns = line.split(/(\s+)/);
+                            var battingStats = {};
+                            var battingStatsIndex = 0;
+                            _.forEach(statsColumns, function(statColumn) {
+                                if (statColumn.trim() !== '') {
+                                    battingStats[battingColumns[battingStatsIndex]] = statColumn;
+                                    battingStatsIndex++;
+                                }
+                            });
+                            jsonStats.teams[teamIndex].batters.push(battingStats);
+
+                            // TODO: determine parser location for batting vs. pitching
+                            // TODO: determine how/when to store team totals
+
+                        }
+                    }
+                }
+            }
+            teamLineFound = false;
         });
+        //console.log(jsonStats);
+        //console.log(jsonStats.teams[0].batters);
         return jsonStats;
     },
     readFromFile: function(fileLocation) {
