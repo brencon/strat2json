d772bb3acfe1a9c0f8a17fb3a263ddf404a634c3
Brendan Conrad <brconrad@symitar.com>
2017-02-23 20:46:15 -0800

    Added test data file, initial file data processing, and updated tests

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var _ = require('lodash');
+
+const fs = require('fs');
+
 /**
  * primaryStats2json
  * Expects data to be sent that is read from a Strat-O-Matic
@@ -10,6 +14,21 @@
 
 module.exports = {
     primaryStats2json: function(primaryStatsPRT) {
-        return primaryStatsPRT;
+        var lines = primaryStatsPRT.split(/\r?\n/);
+        var jsonStats = { "teams": [] };
+        _.forEach(lines, function(line) {
+            // find each team based on "Primary Player Statistics"
+            var primaryPlayerStatistics = 'Primary Player Statistics For 2016 ';
+            if (line.indexOf(primaryPlayerStatistics) > 0) {
+                var teamLine = line.substr(38);
+                var totalsAfterGames = ' Totals After';
+                var team = teamLine.substr(0, teamLine.indexOf(totalsAfterGames));
+                jsonStats.teams.push({"team": team});
+            }
+        });
+        return jsonStats;
+    },
+    readFromFile: function(fileLocation) {
+        return fs.readFileSync(fileLocation, 'utf8');
     }
 };
--- test/test.js
+++ test/test.js
@@ -4,9 +4,25 @@ var expect = require('chai').expect;
 
 var strat2json = require('../index');
 
+const fs = require('fs');
+
+const testDataFileLocation = './test/data/primary-stats-all-teams.prt';
+
+var primaryStatsAllTeamsPRT;
+
+beforeEach(function(done) {
+    primaryStatsAllTeamsPRT = strat2json.readFromFile(testDataFileLocation);
+    done();
+});
+
 describe('#strat2json', function() {
-    it('should return itself', function() {
-        var result = strat2json.primaryStats2json('test');
-        expect(result).to.equal('test');
+    it('should be able to read from a file location and not be null or empty', function() {
+        expect(primaryStatsAllTeamsPRT).to.not.be.null;
+        expect(primaryStatsAllTeamsPRT).to.not.be.empty;
     });
+    it('should take the contents of a file and parse the data into JSON', function() {
+        var result = strat2json.primaryStats2json(primaryStatsAllTeamsPRT);
+        expect(result).to.have.property('teams');
+        expect(result.teams).to.have.length.above(0);
+    })
 });
